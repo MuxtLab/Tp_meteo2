5390655fae067a76bd20da8d17e175fdb81e5ac0
gift-wq <anais.ahossi@muxt.io>
2024-07-31 15:36:41 +0100

    Add files via upload

---
+++ src/components/Loader.tsx
@@ -0,0 +1,13 @@
+// Loader.tsx
+import React from 'react';
+import '../Loader.css';
+
+const Loader: React.FC = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Loader;
